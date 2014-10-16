91dca2004a694b6c3835e4079e4a5a3528548c83
Quinten Palmer <quintenpalmer@gmail.com>
2014-10-16 10:53:06 -0400

    Fix warning in primalPredator

--- src/Instances/Classes.hs
+++ src/Instances/Classes.hs
@@ -17,5 +17,8 @@ druid = newClass "Druid" [(Ref, 1), (Will, 1)] (12 + 9) 5
 primalPredator :: ClassSpec
 primalPredator = ClassSpec "Primal Predator"
     (\player ->
-        let miscSpeedPlus = (getSpeedMisc player) ++ [(\player -> if wearingHeavyArmor player then 0 else 1)] in
+        let miscSpeedPlus = (getSpeedMisc player) ++ [oneSpeedIfLightArmor] in
             player { getSpeedMisc = miscSpeedPlus })
+
+oneSpeedIfLightArmor :: Player -> Int
+oneSpeedIfLightArmor player = if wearingHeavyArmor player then 0 else 1
